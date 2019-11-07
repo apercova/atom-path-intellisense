e79503d6cf6c008956f8b1afb08b192c0b454e8f
apercova <alonperez@monex.com.mx>
2019-11-06 20:40:18 -0600

    Updated sample

--- assets/files/sample.js
+++ assets/files/sample.js
@@ -7,3 +7,6 @@ import fs from 'fs'
 import path from 'path'
 import winston from 'winston'
 import sample from './sample.js'
+
+const process = require('process'), cprocess = require('child_process'),
+file01 = require('./../esca\'ped_dir/esca\'ped_file01'), file02 = require('../esca\'ped_dir/esca\'ped_file02');
